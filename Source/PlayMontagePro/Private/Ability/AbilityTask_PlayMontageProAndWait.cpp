05fab2dce2acb199601e3e2b2e31a7bcc0077a91
Jared Taylor <jaredtaylor.99@gmail.com>
2025-07-07 09:51:58 +1200

    OnDestroy EnsureBroadcastNotifyEvents

--- Source/PlayMontagePro/Private/Ability/AbilityTask_PlayMontageProAndWait.cpp
+++ Source/PlayMontagePro/Private/Ability/AbilityTask_PlayMontageProAndWait.cpp
@@ -290,6 +290,8 @@ void UAbilityTask_PlayMontageProAndWait::OnTickPose(USkinnedMeshComponent* Skinn
 
 void UAbilityTask_PlayMontageProAndWait::OnDestroy(bool AbilityEnded)
 {
+	UPlayMontageProStatics::EnsureBroadcastNotifyEvents(EAnimNotifyProEventType::OnCompleted, Notifies, this);
+	
 	if (TickPoseHandle.IsValid() && GetMesh())
 	{
 		if (TickPoseHandle.IsValid() && GetMesh()->OnTickPose.IsBoundToObject(this))
